70d6d0aeee1de01ef235e3ef18366d2f427833d4
Umarova Albina <albinaumarova2014@gmail.com>
2026-02-17 14:38:15 +0600

    Init app

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import {Provider } from "react-redux"
+import { store } from "./app/store.ts"
+import { ThemeProvider } from "@mui/material"
+import {BrowserRouter} from "react-router-dom";
+import theme from "./theme.ts";
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
+    </Provider>
+)
